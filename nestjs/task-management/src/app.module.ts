509488002e4f2dc16c66cf7a1e3d1ed0f0414adb
Ivo Costa <costa_ivo@yahoo.com>
2020-06-21 00:56:28 +0530

    Initial code for nestjs project

---
+++ nestjs/task-management/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+
+
+@Module({
+  imports: [TasksModule],
+  providers: [],
+})
+
+
+
+
+
+
+export class AppModule { }
