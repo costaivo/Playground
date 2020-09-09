af71e61e2af260f3647678dd597e27d6d6cddcf6
Ivo Costa <costa_ivo@yahoo.com>
2020-09-10 00:09:55 +0530

    added DTO

--- nestjs/task-mgm/src/tasks/tasks.controller.ts
+++ nestjs/task-mgm/src/tasks/tasks.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { title } from 'process';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService){
+    constructor(private tasksService: TasksService) {
     }
 
     @Get()
-    getAllTasks():Task[]{
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto:CreateTaskDto) {
+        return this.tasksService.createTask(createTaskDto);
     }
 }
