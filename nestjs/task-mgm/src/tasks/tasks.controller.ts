f3c9480cc267db7a82e2318dcb471c250704fcc0
Ivo Costa <costa_ivo@yahoo.com>
2020-09-10 20:53:08 +0530

    Added search filters

--- nestjs/task-mgm/src/tasks/tasks.controller.ts
+++ nestjs/task-mgm/src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Body, Post, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Body, Post, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,7 +10,10 @@ export class TasksController {
     }
 
     @Get()
-    getAllTasks(): Task[] {
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if(Object.keys(filterDto).length){
+            return this.tasksService.getTasksWithFilters(filterDto);
+        }
         return this.tasksService.getAllTasks();
     }
     @Get('/:id')
