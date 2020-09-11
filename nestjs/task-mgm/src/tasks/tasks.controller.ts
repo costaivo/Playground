598f26fb94ad4016b60b9ddf8c0935eb81b27d0f
Ivo Costa <costa_ivo@yahoo.com>
2020-09-12 00:23:16 +0530

    Implemented pipes

--- nestjs/task-mgm/src/tasks/tasks.controller.ts
+++ nestjs/task-mgm/src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Body, Post, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Body, Post, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,7 +11,7 @@ export class TasksController {
     }
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
         if(Object.keys(filterDto).length){
             return this.tasksService.getTasksWithFilters(filterDto);
         }
@@ -22,6 +23,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto) {
         return this.tasksService.createTask(createTaskDto);
     }
@@ -32,7 +34,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTask(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus
+        @Body('status',TaskStatusValidationPipe) status: TaskStatus
     ): Task {
         return this.tasksService.updateTaskStatus(id, status);
     }
