5ad735f5ac612e81a5f5c2c9517892eeb8ab8486
Ivo Costa <costa_ivo@yahoo.com>
2020-10-02 17:30:01 +0530

    Added signup route

--- nestjs/task-mgm/src/auth/auth.controller.ts
+++ nestjs/task-mgm/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {
+
+
+    }
+
+    @Post('/signUp')
+    async signUp(@Body() authCredentialDto: AuthCredentialDto) {
+        return this.authService.signUp(authCredentialDto);
+    }
+}
--- nestjs/task-mgm/src/auth/auth.service.ts
+++ nestjs/task-mgm/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+        ) {}
+
+        async signUp(authCredentialDto:AuthCredentialDto):Promise<void>{
+            return this.userRepository.signUp(authCredentialDto);
+        }
+}
