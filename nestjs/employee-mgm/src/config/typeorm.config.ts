34de86fcd1dc88dccef945e74ec92753a4272d02
Ivo Costa <costa_ivo@yahoo.com>
2020-10-13 14:27:10 +0530

    NEST-105: Documentation

--- nestjs/employee-mgm/src/config/typeorm.config.ts
+++ nestjs/employee-mgm/src/config/typeorm.config.ts
@@ -1,6 +1,15 @@
 
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+/**
+* Configuration file for TypeOrm Module
+*
+* Set the Database Type and connection details 
+*
+* Set the entities path 
+*
+* Set the synchronize option. If true, will update the database automatically.
+*/
 export const TypeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
--- nestjs/employee-mgm/src/main.ts
+++ nestjs/employee-mgm/src/main.ts
@@ -2,9 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+/**
+ * Bootstrap the nest-js application
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Setup Documentation
+  documentationSetup(app);
+
+  await app.listen(4200);
+}
+
+/**
+ * Add API documentation for API endPoints
+ * 
+ * @param app Nest Application
+ */
+function documentationSetup(app){
   const options = new DocumentBuilder()
   .setTitle('Training Tracker')
   .setDescription('The API for Training Tracker Application')
@@ -12,7 +27,6 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, options);
 SwaggerModule.setup('api', app, document);
-
-  await app.listen(4200);
 }
+
 bootstrap();
