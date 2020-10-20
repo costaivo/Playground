ef64ebde6f0f6b2cb5cb68df486a6d36eb14c5a1
Costa Ivo <costa_ivo@yahoo.com>
2020-10-20 20:10:11 +0530

    Auth Stratergy

--- nestjs/task-mgm/src/auth/auth.controller.ts
+++ nestjs/task-mgm/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { JwtPayload } from './jwt-payload.interface';
@@ -10,12 +11,18 @@ export class AuthController {
     }
 
     @Post('/signUp')
-    async signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto):Promise<void> {
+    async signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.authService.signUp(authCredentialDto);
     }
 
     @Post('/signIn')
-    async signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto):Promise<{accessToken:string}>{
+    async signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialDto);
+    }
+
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    async test(@Req() req) {
+        console.log(req);
     }
 }
