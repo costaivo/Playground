a5ac0c8d53fcaca21e0c0b3c03f400fb5cb89ccb
Costa Ivo <costa_ivo@yahoo.com>
2021-01-30 00:19:15 +0530

    configured nodemon

--- nodejs/udemy/notes-app/app.js
+++ nodejs/udemy/notes-app/app.js
@@ -3,5 +3,5 @@ const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 console.log(chalk.bold.green('I am a green text'));
-console.log(chalk.green('I am a green text'));
+console.log(chalk.red.bold.italic('This is a warning!!'));
 console.log(getNotes());
