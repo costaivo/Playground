cd0088be915ff9e8d483f81c192c3e1ed8391d90
Costa Ivo <costa_ivo@yahoo.com>
2021-01-30 00:04:40 +0530

    Added gitignore file

--- nodejs/udemy/notes-app/app.js
+++ nodejs/udemy/notes-app/app.js
@@ -1,3 +1,6 @@
+const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
+console.log(chalk.green('I am a green text'));
 console.log(getNotes());
