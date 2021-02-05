fd20078d3df8491eb04751c2e1fcd6ee4c4fcf46
Costa Ivo <costa_ivo@yahoo.com>
2021-02-05 17:19:22 +0530

    Added add and listing logic

--- nodejs/udemy/notes-app/app.js
+++ nodejs/udemy/notes-app/app.js
@@ -1,55 +1,56 @@
 const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Application version
 yargs.version("1.0.0");
 // Add command
 yargs.command({
-    command:'add',
-    description:'Add a new note',
-    builder:{
-        title:{
-            descripe:'Note title',
-            demandOption:true,
-            type:'string'
+    command: 'add',
+    description: 'Add a new note',
+    builder: {
+        title: {
+            descripe: 'Note title',
+            demandOption: true,
+            type: 'string'
         },
-        body:{
-descripe:'Note contents',
-demandOption:true,
-type:'string'
+        body: {
+            descripe: 'Note contents',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler:function(argv){
-        console.log('Adding a new note:',argv);
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 });
 
 
 // Remove command
 yargs.command({
-    command:'remove',
-    description:'removes a note',
-    handler:function(){
+    command: 'remove',
+    description: 'removes a note',
+    handler: function () {
         console.log("removes a note");
     }
 });
 
 // list command
 yargs.command({
-    command:'list',
-    description:'list all notes',
-    handler:function(){
-        console.log("listing all note");
+    command: 'list',
+    description: 'list all notes',
+    handler: function () {
+        const note = notes.getNotes();
+        console.log(note);
     }
 });
 
 // read command
 yargs.command({
-    command:'read',
-    description:'read a notes',
-    handler:function(){
+    command: 'read',
+    description: 'read a notes',
+    handler: function () {
         console.log("reading  note");
     }
 });
